6638458766361b2b3ed16df4b0627be6cb48b5da
NinKorn <15377619772@163.com>
2019-05-29 14:45:39 +0800

    创建标记和中心

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,10 +27,10 @@ export default {
   data () {
     return {
       zoom: 13,
-      center: L.latLng(47.413220, -0.219482),
+      center: L.latLng(47.413220, -0.219482),//114.3367123603821,
       url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: L.latLng(47.413220, -1.219482),
+      marker: L.latLng(47.413220, -0.219482),//标记
       text: 'this is a marker'
     }
   }
--- src/main.js
+++ src/main.js
@@ -7,7 +7,12 @@ import router from './router'
 import 'leaflet/dist/leaflet.css';
 
 Vue.config.productionTip = false
-
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
